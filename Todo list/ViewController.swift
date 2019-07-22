1e18947a0f03cba259fb64c2b626aebc91750e2b
IgorNfromK <ccna4igor@gmail.com>
2019-07-22 18:26:06 +0200

    Initial Commit

---
+++ Todo list/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo list
+//
+//  Created by Igor Nagaievskyi on 22/07/2019.
+//  Copyright © 2019 Igor Nagaievskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
